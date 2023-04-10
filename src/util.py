3e28695046be9ac7be80818d292d05e954ce02fd
Teuku Ghaisa Aufa <tg.aufa@gmail.com>
2023-04-10 20:24:43 +0700

    continue feature eng & selection

--- src/util.py
+++ src/util.py
@@ -1,6 +1,7 @@
 import yaml
 import joblib
 from datetime import datetime
+import pandas as pd
 
 config_dir = "config/config.yaml"
 
@@ -34,3 +35,22 @@ def print_debug(messages: str) -> None:
     # Check whether user wants to use print
     if PRINT_DEBUG == True:
         print(time_stamp(), messages)
+
+# Check data statistics, since the column qty is a lot, then we summarize the describe feature in following function
+def summary_dataset_describe(dataset):
+
+    # Get the date index statistic info
+    date_df = pd.Series(dataset.index).describe(include='datetime64', datetime_is_numeric=True)
+
+    df = dataset.describe()
+    # Get the minimum value for each row across all columns and convert it to a DataFrame
+    row_min_df = df.min(axis=1).to_frame('Min')
+
+    # Get the maximum value for each row across all columns and convert it to a DataFrame
+    row_max_df = df.max(axis=1).to_frame('Max')
+
+    # Concatenate the min and max DataFrames horizontally
+    result_df = pd.concat([date_df, row_min_df, row_max_df], axis=1)
+
+
+    return result_df
